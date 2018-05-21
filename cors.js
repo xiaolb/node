ef18135edf3af9ceff99beade81c03e0f78e8143
xiaolb <15990357176@163.com>
2018-05-21 17:35:51 +0800

    rebuild

--- cors.js
+++ cors.js
@@ -3,4 +3,4 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});  
     response.end('Hello World\n');  
 }).listen(80);  
-console.log('Server running at http://127.0.0.1:8888/');  
+console.log('Server running at http://127.0.0.1:80/');  
