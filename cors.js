3cfe59465d1a303b5914ccbe4ef4fbf6c427d824
xiaolb <15990357176@163.com>
2018-05-23 16:30:09 +0800

    解决乱码问题

--- cors.js
+++ cors.js
@@ -1,6 +1,9 @@
 var http = require('http');  
 http.createServer(function (request, response) {  
-    response.writeHead(200, {'Content-Type': 'text/plain'});  
+    response.writeHead(200, {
+      'Content-Type': 'text/plain',
+      'charset': 'utf-8'
+    });  
     response.end('好了哦\n');  
 }).listen(80);  
 console.log('Server running at http://127.0.0.1:80/');  
