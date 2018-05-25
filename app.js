26bf6a77c15b9e8789adf46ac6fdf138307b8529
xiaolibin@xinguangnet.com <15990357176@163.com>
2018-05-25 14:57:06 +0800

    解决跨域问题

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ let bodyParser = require('body-parser');
 // 创建app应用
 let app = express();
 
+app.all('*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true); //可以带cookies
+  res.header("X-Powered-By", '3.2.1')
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  };
+});
+
+
 app.use('/public', express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
