89fe929e1fe1b7a29a7f94de9bc8449ffae5aa52
xiaolibin@xinguangnet.com <15990357176@163.com>
2018-05-25 15:55:25 +0800

    数据库的连接表

--- public/js/index.js
+++ public/js/index.js
@@ -50,9 +50,6 @@ $(function() {
     $.ajax({
       method: 'POST',
       url: 'http://120.77.144.188/api/user/login',
-      headers: {
-        Accept: "application/json; charset=utf-8"
-      },
       data:{
         username,
         password
@@ -77,9 +74,6 @@ $(function() {
     $.ajax({
       method: 'POST',
       url: 'http://120.77.144.188/api/user/show',
-      headers: {
-        Accept: "application/json; charset=utf-8"
-      },
       data:{},
       dataType: 'json',
       success: function(result) {
